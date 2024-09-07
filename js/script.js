f36f92ace0e4e7beafb3d14fe19f6fb461fb8a27
Krymvee <zike001.2005@gmail.com>
2024-09-07 14:25:09 +0700

    finishing style.css dan index.html, menambahkan fungsi switch page pada script.css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// ini script
+let segitiga = document.getElementById('segitiga');
+let jajarGenjang = document.getElementById('jajar-genjang');
+let segitigaBtn = document.getElementById('segitiga-btn');
+let jajarGenjangBtn = document.getElementById('jajar-genjang-btn');
+
+function segitigaActive(){
+    segitiga.style.zIndex = '2';
+}
+
+function jajarGenjangActive(){
+    segitiga.style.zIndex = '1';
+}
+
+
+
+
