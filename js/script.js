ec79aac91d8f9eb17e13b2e5bcc54688ad87469f
Krymvee <zike001.2005@gmail.com>
2024-09-08 12:03:12 +0700

    menambahkan fungsi operasi hitung luas segitiga ke file script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ function segitigaActive() {
     document.getElementById('jajar-genjang').style.zIndex = '1';
     
 }
+
+function luasSegitiga(){
+    var alasInput = document.getElementById('alas-input').value;
+    var tinggiInput = document.getElementById('tinggi-input').value;
+    var hasilLuas = 1/2 * alasInput * tinggiInput;
+
+    document.getElementById('result-luas-segitiga').innerHTML = `Hasilnya yaitu : ${hasilLuas}`;
+}
+
+function reset(){
+    location.reload();
+}
